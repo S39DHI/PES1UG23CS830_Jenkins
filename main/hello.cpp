10430378249a5869e5b38dcae80c0ddf0e6d1136
S39DHI <s39dhi@gmail.com>
2025-03-18 16:26:00 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "hello i am pes1ug23cs830 \n";
   return 0;
 }
